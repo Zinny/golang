8d32820507c7c3f8836c8a9a619ebad61fe93fa7
Cinny Abraham <cinnyabraham06@gmail.com>
2022-07-25 00:24:47 +0000

    second

---
+++ second.go
@@ -0,0 +1,17 @@
+package main 
+
+import "fmt"
+
+/*
+
+* 1. Variables
+* 2. Variables with initializer
+* 3. Short variable declaration 
+
+*/
+
+var x, y, z int
+func main() {
+  fmt.Println(x, y, z)
+ }
+
