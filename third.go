76f09140ea31c8c5c79b57c7a9bb31abc569e89f
Cinny Abraham <cinnyabraham06@gmail.com>
2022-07-25 00:28:51 +0000

    third

---
+++ third.go
@@ -0,0 +1,19 @@
+package main 
+
+import "fmt"
+
+/*
+
+* 1. Variables
+* 2. Variables with initializer
+* 3. Short variable declaration 
+
+*/
+
+var x, y, z  bool
+func main() {
+  var a = 10
+  b := 20
+  fmt.Println(a, b, x, y, z)
+ }
+
